da43264d15137a62674a54603b5f6de8a6164058
Devis <80535814+devistollon@users.noreply.github.com>
2022-06-15 14:37:12 +0200

    Changed paho library with Azure SDK

--- simulator.py
+++ simulator.py
@@ -1,35 +1,30 @@
 # Composizione del JSON: deviceId, timeStamp,  walkCount, heartFrq, bloodOxigen, isSleeping
-import paho.mqtt.client as mqtt
+from azure.iot.device.aio import IoTHubDeviceClient
 from dotenv import load_dotenv
 from datetime import datetime
+import asyncio
 import os
-import ssl
 import random
 import time
 import json
+import warnings
+warnings.filterwarnings('ignore')
+load_dotenv(dotenv_path=os.path.join(os.getcwd(), '.env'))
 
-env_path = os.path.join(os.getcwd(), '.env')
-load_dotenv(dotenv_path=env_path)
 
 idList = ['1', '2']
 d = random.choice(idList)
 
-path_to_root_cert = os.getenv("PATH_TO_ROOT_CERT")
-cert_file = os.getenv("CERT_FILE")
-key_file = os.getenv("KEY_FILE")
-device_id = os.getenv("DEVICE_ID")
-iot_hub_name = os.getenv("IOT_HUB_NAME")
+client = IoTHubDeviceClient.create_from_connection_string(os.getenv("CONNECTION_STRING"))
+client.connect()
 
-def on_connect(client, userdata, flags, rc):
-    print("Device connected with result code: " + str(rc))
-
-
-def on_disconnect(client, userdata, rc):
-    print("Device disconnected with result code: " + str(rc))
-
-
-def on_publish(client, userdata, mid):
-    print("Device sent message")
+async def sendMessage(jsonData: str):
+    print('Sending message...')
+    try:
+        await client.send_message(jsonData)
+        print(f'Sent\n{jsonData}')
+    except:
+        print('Your message could not be sent')
 
 def generateData(devId: int):
     isSleeping = False
@@ -49,17 +44,6 @@ def generateData(devId: int):
     }
     return json.dumps(jsonData)
 
-client = mqtt.Client(client_id=device_id, protocol=mqtt.MQTTv311)
-
-client.on_connect = on_connect
-client.on_disconnect = on_disconnect
-client.on_publish = on_publish
-
-client.username_pw_set(username=iot_hub_name+".azure-devices.net/" + device_id + "/?api-version=2021-04-12", password=None)
-
-client.tls_set(ca_certs=path_to_root_cert, certfile=cert_file, keyfile=key_file, cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_TLSv1_2, ciphers=None)
-client.tls_insecure_set(False)
-
-client.connect(iot_hub_name+".azure-devices.net", port=8883)
-client.publish("devices/" + device_id + "/messages/events/", generateData(d), qos=1)
-client.loop_forever()
+while True:
+    asyncio.run(sendMessage(generateData(d)))
+    time.sleep(10)
